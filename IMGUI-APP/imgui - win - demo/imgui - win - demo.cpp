3cefdbff5a9c4809eb18e08f3c9527a67f234ffd
POMIN-163 <1637766030@QQ.com>
2020-10-16 10:25:54 +0800

    2020-10-16

--- IMGUI-APP/imgui - win - demo/imgui - win - demo.cpp
+++ IMGUI-APP/imgui - win - demo/imgui - win - demo.cpp
@@ -10,98 +10,21 @@
 
 #pragma comment ( lib, "D3D11.lib")
 
+#pragma region D3D_var
+
 static ID3D11Device*            g_pd3dDevice = NULL;
 static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
 static IDXGISwapChain*          g_pSwapChain = NULL;
 static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
 
+#pragma endregion
+
+#pragma region D3D_Function
+
 bool CreateDeviceD3D(HWND hWnd);
 void CleanupDeviceD3D();
 void CreateRenderTarget();
 void CleanupRenderTarget();
-
-void ShowWindow(bool* p_open);
-
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR    lpCmdLine,
-	_In_ int       nCmdShow)
-{
-	WNDCLASSEX wc = { 
-		sizeof(WNDCLASSEX),
-		CS_CLASSDC,
-		WndProc,
-		0L, 0L,
-		hInstance,
-		LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON3)),
-		LoadCursor(nullptr, IDC_ARROW),
-		(HBRUSH)(COLOR_WINDOW + 1),
-		nullptr,
-		_T("ImGui Example"),
-		LoadIcon(wc.hInstance, MAKEINTRESOURCE(IDI_APPLICATION))
-	};
-	::RegisterClassEx(&wc);
-	HWND hwnd = ::CreateWindow(
-		wc.lpszClassName, 
-		_T("ImGui - win"), 
-		WS_EX_TOPMOST | WS_OVERLAPPEDWINDOW, 
-		100, 100, 600, 400, 
-		NULL, NULL, wc.hInstance, NULL
-	);
-
-	if (!CreateDeviceD3D(hwnd))
-	{
-		CleanupDeviceD3D();
-		::UnregisterClass(wc.lpszClassName, wc.hInstance);
-		return 1;
-	}
-	::ShowWindow(hwnd, SW_SHOWDEFAULT);
-	::UpdateWindow(hwnd);
-
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	(void)io;
-	ImGui::StyleColorsDark();
-	ImGui_ImplWin32_Init(hwnd);
-	ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
-
-	io.Fonts->AddFontDefault();
-
-	bool pomin_window = true;
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-	MSG msg;
-	ZeroMemory(&msg, sizeof(msg));
-	while (msg.message != WM_QUIT) {
-		if (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE)) {
-			::TranslateMessage(&msg);
-			::DispatchMessage(&msg);
-			continue;
-		}
-		ImGui_ImplDX11_NewFrame();
-		ImGui_ImplWin32_NewFrame();
-		ImGui::NewFrame();
-
-		if (pomin_window) {
-			ShowWindow(&pomin_window);// my window
-		}
-
-		ImGui::Render();
-		g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
-		g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, (float*)&clear_color);
-		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-		g_pSwapChain->Present(1, 0);
-	}
-	ImGui_ImplDX11_Shutdown();
-	ImGui_ImplWin32_Shutdown();
-	ImGui::DestroyContext();
-	CleanupDeviceD3D();
-	::DestroyWindow(hwnd);
-	::UnregisterClass(wc.lpszClassName, wc.hInstance);
-
-	return 0;
-}
 
 bool CreateDeviceD3D(HWND hWnd)
 {
@@ -150,7 +73,15 @@ void CleanupRenderTarget()
 {
 	if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
 }
+#pragma endregion
 
+void ShowWindow(bool* p_open);
+
+#pragma region Win_Fun
+
+
+
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -178,9 +109,115 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	}
 	return ::DefWindowProc(hWnd, msg, wParam, lParam);
 }
+#pragma endregion
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{ 
+	WNDCLASSEX wc = { 
+		sizeof(WNDCLASSEX),
+		CS_CLASSDC,
+		WndProc,
+		0L, 0L,
+		hInstance,
+		LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON3)),
+		LoadCursor(nullptr, IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		nullptr,
+		_T("ImGui Example"),
+		LoadIcon(wc.hInstance, MAKEINTRESOURCE(IDI_APPLICATION))
+	};
+	::RegisterClassEx(&wc);
+	HWND hwnd = ::CreateWindow(
+		wc.lpszClassName, 
+		_T("ImGui - win"), 
+		WS_EX_TOPMOST | WS_OVERLAPPEDWINDOW, 
+		100, 100, 600, 400, 
+		NULL, NULL, wc.hInstance, NULL
+	);
+
+	if (!CreateDeviceD3D(hwnd)) {
+		CleanupDeviceD3D();
+		::UnregisterClass(wc.lpszClassName, wc.hInstance);
+		return 1;
+	}
+	::ShowWindow(hwnd, SW_SHOWDEFAULT);
+	::UpdateWindow(hwnd);
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	(void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplWin32_Init(hwnd);
+	ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+
+	// io.Fonts->AddFontDefault();
+	io.Fonts->AddFontFromFileTTF("c:\\windows\\fonts\\msyh.ttc", 16.0f, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
+
+	bool pomin_window = true;
+	bool demo_window = true;
+
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+	while (msg.message != WM_QUIT) {
+		if (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE)) {
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+			continue;
+		}
+
+#pragma region ImGui_Init
+
+		ImGui_ImplDX11_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+
+#pragma endregion
+
+		if (pomin_window) {
+			ShowWindow(&pomin_window);// my window
+		}
+		if (demo_window) {
+			ImGui::ShowDemoWindow(&demo_window);
+		}
+
+#pragma region ImGui_Refresh
+		ImGui::Render();
+		g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+		g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, (float*)&clear_color);
+		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+		g_pSwapChain->Present(1, 0);
+#pragma endregion
+	}
+#pragma region D3D_End
+
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+	CleanupDeviceD3D();
+	::DestroyWindow(hwnd);
+	::UnregisterClass(wc.lpszClassName, wc.hInstance);
+
+#pragma endregion
+	return 0;
+}
 
 void ShowWindow(bool* p_open) {
-	ImGui::Begin("POMIN", p_open);
+
+	ImGui::Begin(u8"POMIN 的窗口", p_open, ImGuiWindowFlags_NoTitleBar|ImGuiWindowFlags_MenuBar);
+	if (ImGui::BeginMenuBar()) {
+		if (ImGui::BeginMenu("Menu1")) {
+			if (ImGui::MenuItem("Item1", "", false)) {
+				MessageBoxA(NULL, "OK", "TIP", 0);
+			}
+		ImGui::EndMenu();
+		}
+	ImGui::EndMenuBar();
+	}
+
 	ImGui::Text("pomin's first window");
 	if (ImGui::Button("my button")) {
 		MessageBoxA(NULL, "OK", "TIP", 0);
