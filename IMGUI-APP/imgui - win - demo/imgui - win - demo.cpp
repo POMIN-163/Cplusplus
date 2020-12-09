7ca13bb390382eb3e044d0793d8441346d3ffdce
POMIN-163 <1637766030@QQ.com>
2020-12-09 11:56:32 +0800

    2020-12-9

--- IMGUI-APP/imgui - win - demo/imgui - win - demo.cpp
+++ IMGUI-APP/imgui - win - demo/imgui - win - demo.cpp
@@ -12,25 +12,24 @@
 
 #pragma region D3D_var
 
-static ID3D11Device*            g_pd3dDevice = NULL;
-static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
-static IDXGISwapChain*          g_pSwapChain = NULL;
-static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
+static ID3D11Device* g_pd3dDevice = NULL;
+static ID3D11DeviceContext* g_pd3dDeviceContext = NULL;
+static IDXGISwapChain* g_pSwapChain = NULL;
+static ID3D11RenderTargetView* g_mainRenderTargetView = NULL;
 
 #pragma endregion
 
 #pragma region D3D_Function
 
-bool CreateDeviceD3D(HWND hWnd);
-void CleanupDeviceD3D();
-void CreateRenderTarget();
-void CleanupRenderTarget();
+bool CreateDeviceD3D (HWND hWnd);
+void CleanupDeviceD3D ();
+void CreateRenderTarget ();
+void CleanupRenderTarget ();
 
-bool CreateDeviceD3D(HWND hWnd)
-{
+bool CreateDeviceD3D (HWND hWnd) {
 	// Setup swap chain
 	DXGI_SWAP_CHAIN_DESC sd;
-	ZeroMemory(&sd, sizeof(sd));
+	ZeroMemory (&sd, sizeof (sd));
 	sd.BufferCount = 2;
 	sd.BufferDesc.Width = 0;
 	sd.BufferDesc.Height = 0;
@@ -49,54 +48,46 @@ bool CreateDeviceD3D(HWND hWnd)
 	//createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
 	D3D_FEATURE_LEVEL featureLevel;
 	const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
-	if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
+	if (D3D11CreateDeviceAndSwapChain (NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
 		return false;
 
-	CreateRenderTarget();
+	CreateRenderTarget ();
 	return true;
 }
-void CleanupDeviceD3D()
-{
-	CleanupRenderTarget();
-	if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
-	if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = NULL; }
-	if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
+void CleanupDeviceD3D () {
+	CleanupRenderTarget ();
+	if (g_pSwapChain) { g_pSwapChain->Release (); g_pSwapChain = NULL; }
+	if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release (); g_pd3dDeviceContext = NULL; }
+	if (g_pd3dDevice) { g_pd3dDevice->Release (); g_pd3dDevice = NULL; }
 }
-void CreateRenderTarget()
-{
+void CreateRenderTarget () {
 	ID3D11Texture2D* pBackBuffer;
-	g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
-	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
-	pBackBuffer->Release();
+	g_pSwapChain->GetBuffer (0, IID_PPV_ARGS (&pBackBuffer));
+	g_pd3dDevice->CreateRenderTargetView (pBackBuffer, NULL, &g_mainRenderTargetView);
+	pBackBuffer->Release ();
 }
-void CleanupRenderTarget()
-{
-	if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
+void CleanupRenderTarget () {
+	if (g_mainRenderTargetView) { g_mainRenderTargetView->Release (); g_mainRenderTargetView = NULL; }
 }
 #pragma endregion
 
-void ShowWindow(bool* p_open);
+void ShowWindow (bool* p_open);
 
 #pragma region Win_Fun
 
+LRESULT WINAPI WndProc (HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler (HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+LRESULT WINAPI WndProc (HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	if (ImGui_ImplWin32_WndProcHandler (hWnd, msg, wParam, lParam))
 		return true;
 
-	switch (msg)
-	{
+	switch (msg) {
 	case WM_SIZE:
-		if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
-		{
-			CleanupRenderTarget();
-			g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
-			CreateRenderTarget();
+		if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED) {
+			CleanupRenderTarget ();
+			g_pSwapChain->ResizeBuffers (0, (UINT)LOWORD (lParam), (UINT)HIWORD (lParam), DXGI_FORMAT_UNKNOWN, 0);
+			CreateRenderTarget ();
 		}
 		return 0;
 	case WM_SYSCOMMAND:
@@ -104,123 +95,121 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			return 0;
 		break;
 	case WM_DESTROY:
-		::PostQuitMessage(0);
+		::PostQuitMessage (0);
 		return 0;
 	}
-	return ::DefWindowProc(hWnd, msg, wParam, lParam);
+	return ::DefWindowProc (hWnd, msg, wParam, lParam);
 }
 #pragma endregion
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+//
+int APIENTRY wWinMain (_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
-	_In_ int       nCmdShow)
-{ 
-	WNDCLASSEX wc = { 
-		sizeof(WNDCLASSEX),
+	_In_ int       nCmdShow) {
+	WNDCLASSEX wc = {
+		sizeof (WNDCLASSEX),
 		CS_CLASSDC,
 		WndProc,
 		0L, 0L,
 		hInstance,
-		LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON3)),
-		LoadCursor(nullptr, IDC_ARROW),
+		LoadIcon (hInstance, MAKEINTRESOURCE (IDI_ICON3)),
+		LoadCursor (nullptr, IDC_ARROW),
 		(HBRUSH)(COLOR_WINDOW + 1),
 		nullptr,
-		_T("ImGui Example"),
-		LoadIcon(wc.hInstance, MAKEINTRESOURCE(IDI_APPLICATION))
+		_T ("ImGui Example"),
+		LoadIcon (wc.hInstance, MAKEINTRESOURCE (IDI_APPLICATION))
 	};
-	::RegisterClassEx(&wc);
-	HWND hwnd = ::CreateWindow(
-		wc.lpszClassName, 
-		_T("ImGui - win"), 
-		WS_EX_TOPMOST | WS_OVERLAPPEDWINDOW, 
-		100, 100, 600, 400, 
+	::RegisterClassEx (&wc);
+	HWND hwnd = ::CreateWindow (
+		wc.lpszClassName,
+		_T ("ImGui - win"),
+		WS_EX_TOPMOST | WS_OVERLAPPEDWINDOW,
+		100, 100, 600, 400,
 		NULL, NULL, wc.hInstance, NULL
 	);
 
-	if (!CreateDeviceD3D(hwnd)) {
-		CleanupDeviceD3D();
-		::UnregisterClass(wc.lpszClassName, wc.hInstance);
+	if (!CreateDeviceD3D (hwnd)) {
+		CleanupDeviceD3D ();
+		::UnregisterClass (wc.lpszClassName, wc.hInstance);
 		return 1;
 	}
-	::ShowWindow(hwnd, SW_SHOWDEFAULT);
-	::UpdateWindow(hwnd);
+	::ShowWindow (hwnd, SW_SHOWDEFAULT);
+	::UpdateWindow (hwnd);
 
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
+	ImGui::CreateContext ();
+	ImGuiIO& io = ImGui::GetIO ();
 	(void)io;
-	ImGui::StyleColorsDark();
-	ImGui_ImplWin32_Init(hwnd);
-	ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+	ImGui::StyleColorsDark ();
+	ImGui_ImplWin32_Init (hwnd);
+	ImGui_ImplDX11_Init (g_pd3dDevice, g_pd3dDeviceContext);
 
 	// io.Fonts->AddFontDefault();
-	io.Fonts->AddFontFromFileTTF("c:\\windows\\fonts\\msyh.ttc", 16.0f, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
+	io.Fonts->AddFontFromFileTTF ("c:\\windows\\fonts\\msyh.ttc", 16.0f, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon ());
 
 	bool pomin_window = true;
 	bool demo_window = true;
 
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	ImVec4 clear_color = ImVec4 (0.45f, 0.55f, 0.60f, 1.00f);
 	MSG msg;
-	ZeroMemory(&msg, sizeof(msg));
+	ZeroMemory (&msg, sizeof (msg));
 	while (msg.message != WM_QUIT) {
-		if (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE)) {
-			::TranslateMessage(&msg);
-			::DispatchMessage(&msg);
+		if (::PeekMessage (&msg, NULL, 0U, 0U, PM_REMOVE)) {
+			::TranslateMessage (&msg);
+			::DispatchMessage (&msg);
 			continue;
 		}
 
 #pragma region ImGui_Init
 
-		ImGui_ImplDX11_NewFrame();
-		ImGui_ImplWin32_NewFrame();
-		ImGui::NewFrame();
+		ImGui_ImplDX11_NewFrame ();
+		ImGui_ImplWin32_NewFrame ();
+		ImGui::NewFrame ();
 
 #pragma endregion
 
 		if (pomin_window) {
-			ShowWindow(&pomin_window);// my window
+			ShowWindow (&pomin_window);// my window
 		}
 		if (demo_window) {
-			ImGui::ShowDemoWindow(&demo_window);
+			ImGui::ShowDemoWindow (&demo_window);
 		}
 
 #pragma region ImGui_Refresh
-		ImGui::Render();
-		g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
-		g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, (float*)&clear_color);
-		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-		g_pSwapChain->Present(1, 0);
+		ImGui::Render ();
+		g_pd3dDeviceContext->OMSetRenderTargets (1, &g_mainRenderTargetView, NULL);
+		g_pd3dDeviceContext->ClearRenderTargetView (g_mainRenderTargetView, (float*)&clear_color);
+		ImGui_ImplDX11_RenderDrawData (ImGui::GetDrawData ());
+		g_pSwapChain->Present (1, 0);
 #pragma endregion
 	}
 #pragma region D3D_End
 
-	ImGui_ImplDX11_Shutdown();
-	ImGui_ImplWin32_Shutdown();
-	ImGui::DestroyContext();
-	CleanupDeviceD3D();
-	::DestroyWindow(hwnd);
-	::UnregisterClass(wc.lpszClassName, wc.hInstance);
+	ImGui_ImplDX11_Shutdown ();
+	ImGui_ImplWin32_Shutdown ();
+	ImGui::DestroyContext ();
+	CleanupDeviceD3D ();
+	::DestroyWindow (hwnd);
+	::UnregisterClass (wc.lpszClassName, wc.hInstance);
 
 #pragma endregion
 	return 0;
 }
 
-void ShowWindow(bool* p_open) {
-
-	ImGui::Begin(u8"POMIN 的窗口", p_open, ImGuiWindowFlags_NoTitleBar|ImGuiWindowFlags_MenuBar);
-	if (ImGui::BeginMenuBar()) {
-		if (ImGui::BeginMenu("Menu1")) {
-			if (ImGui::MenuItem("Item1", "", false)) {
-				MessageBoxA(NULL, "OK", "TIP", 0);
+void ShowWindow (bool* p_open) {
+	ImGui::Begin (u8"POMIN 的窗口", p_open, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_MenuBar);
+	if (ImGui::BeginMenuBar ()) {
+		if (ImGui::BeginMenu ("Menu1")) {
+			if (ImGui::MenuItem ("Item1", "", false)) {
+				MessageBoxA (NULL, "OK", "TIP", 0);
 			}
-		ImGui::EndMenu();
+			ImGui::EndMenu ();
 		}
-	ImGui::EndMenuBar();
+		ImGui::EndMenuBar ();
 	}
 
-	ImGui::Text("pomin's first window");
-	if (ImGui::Button("my button")) {
-		MessageBoxA(NULL, "OK", "TIP", 0);
+	ImGui::Text ("pomin's first window");
+	if (ImGui::Button ("my button")) {
+		MessageBoxA (NULL, "OK", "TIP", 0);
 	}
-	ImGui::End();
+	ImGui::End ();
 }
